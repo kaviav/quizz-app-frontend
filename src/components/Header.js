17e2dfd52fae0cb70d8099439e730171c72603e0
Whale77777 <kavyasathyanathan@gmail.com>
2023-09-13 16:19:15 +0530

    updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
+import logo from "../assets/Quiz-Logo-Transparent-Background.png";
+
 function Header() {
   return (
-    <header className='app-header'>
-      <img src='logo512.png' alt='React logo' />
+    <header className="app-header">
+      <img src={logo} alt="React logo" />
       <h1>The React Quiz</h1>
     </header>
   );
